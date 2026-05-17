00920c2d5bd9df847ab6c2fbab006346c75c6095
Danny Shmueli <dannyshmueli@gmail.com>
2026-05-17 14:57:16 +0300

    style: use image-generated Hermes wings icon

--- src/hermes-icon.ts
+++ src/hermes-icon.ts
@@ -2,36 +2,17 @@ export const HERMES_ICON_ID = "hermes-caduceus-wing";
 export const HERMES_MARK_ICON_ID = "hermes-caduceus-mark";
 
 /**
- * Sidebar/ribbon icon: simple Hermes wings.
- * No caduceus/staff/snakes — those collapse into noise at Obsidian ribbon size.
+ * Image-gen based Hermes wings.
+ * Source concept: /Users/danny/.hermes/cache/images/openai_codex_gpt-image-2-high_20260517_145621_78c77a5a.png
+ * Filled silhouette with simple feather cuts so it reads in Obsidian's tiny ribbon.
  */
 export const HERMES_ICON_SVG = `
-<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12 12.2c-2.2-3.9-5-6.1-8.3-6.7.2 3.6 2.3 6.4 6.2 8.4" />
-  <path d="M7.1 8.3 4.2 8" />
-  <path d="M8.7 10.4 5 10.2" />
-  <path d="M10.2 12.5 6.6 13" />
-  <path d="M12 12.2c2.2-3.9 5-6.1 8.3-6.7-.2 3.6-2.3 6.4-6.2 8.4" />
-  <path d="M16.9 8.3l2.9-.3" />
-  <path d="M15.3 10.4l3.7-.2" />
-  <path d="M13.8 12.5l3.6.5" />
-  <path d="M10 15.2c1.3.8 2.7.8 4 0" />
+<svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M11.25 19.8C9.9 16 6.95 15.55 4.65 13.65 2.45 11.85 1.55 8.85 1.35 5.45c3.05 2.8 6.15 4.55 8.45 6.75 1.55 1.5 2.1 3.45 1.45 7.6ZM4.05 9.35c2.15 1.05 4.05 2.05 5.9 3.75-2.35-.25-4.1-.9-5.9-3.75Zm1.55 4.15c1.55.75 3.05 1.25 4.45 1.55-1.85.55-3.25.1-4.45-1.55Z" />
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.75 19.8c1.35-3.8 4.3-4.25 6.6-6.15 2.2-1.8 3.1-4.8 3.3-8.2-3.05 2.8-6.15 4.55-8.45 6.75-1.55 1.5-2.1 3.45-1.45 7.6Zm7.2-10.45c-2.15 1.05-4.05 2.05-5.9 3.75 2.35-.25 4.1-.9 5.9-3.75Zm-1.55 4.15c-1.55.75-3.05 1.25-4.45 1.55 1.85.55 3.25.1 4.45-1.55Z" />
 </svg>`;
 
 /**
- * Header/brand mark: larger version of the same Hermes wing idea.
+ * Header mark: same generated wing shape, just shown bigger in the plugin chrome.
  */
-export const HERMES_MARK_ICON_SVG = `
-<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.9" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12 12.5c-2.4-4.3-5.4-6.8-9-7.4.2 4 2.5 7.2 6.7 9.5" />
-  <path d="M5.8 7.7 3.5 7.5" />
-  <path d="M7.6 9.7 3.9 9.7" />
-  <path d="M9.3 11.9 5 12.4" />
-  <path d="M10.8 14.1 7.6 15.2" />
-  <path d="M12 12.5c2.4-4.3 5.4-6.8 9-7.4-.2 4-2.5 7.2-6.7 9.5" />
-  <path d="M18.2 7.7l2.3-.2" />
-  <path d="M16.4 9.7h3.7" />
-  <path d="M14.7 11.9l4.3.5" />
-  <path d="M13.2 14.1l3.2 1.1" />
-  <path d="M9.3 16.7c1.8 1.2 3.6 1.2 5.4 0" />
-</svg>`;
+export const HERMES_MARK_ICON_SVG = HERMES_ICON_SVG;
